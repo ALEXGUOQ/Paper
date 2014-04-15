e2b796a1fe623ae27c0fe61d6ab902219bc5e2be
swt <275712575@qq.com>
2014-04-15 16:11:10 +0800

    创建项目

---
+++ Paper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paper
+//
+//  Created by tag_mac_05 on 14-4-15.
+//  Copyright (c) 2014年 ChinaiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Paper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Paper
+//
+//  Created by tag_mac_05 on 14-4-15.
+//  Copyright (c) 2014年 ChinaiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
